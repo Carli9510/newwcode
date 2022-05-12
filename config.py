55b604ff48589376ea66fe6e74ef0ab143ed8f8b
Carli9510 <99306256+Carli9510@users.noreply.github.com>
2022-05-12 16:18:32 -0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,18 @@
+﻿BOT_TOKEN = '5357738313:AAGdkXf2reD20F60mTS-hA06-z_GcLCMlwk'
+API_ID = 
+16295046
+
+API_HASH = '74e87e9ae1d822d0e7872e080ed689a2'
+
+
+# DATOS DEL MOODLE
+USUARIO = 'raguilar'
+PASSWORD = '*AngelBlack*01'
+USUARIO_ID = 12622
+ZIP_MB = 399
+MOODLE_URL = 'moodle.uclv.edu.cu'
+OWNER = [1652377398]
+
+# CUENTA DE MEGA
+GMAIL_MEGA = 'gmail@gmail.com'
+PASS_MEGA = 'contraseña*'
